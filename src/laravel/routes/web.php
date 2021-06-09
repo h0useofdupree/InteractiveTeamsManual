1618c6d648c59f1d3369adf384bc0ddfc79702f0
h0useofdupree <joel.riekemann@gmail.com>
2021-06-09 11:43:50 +0200

    renamed 'rawManualFiles' to 'pdfManuals'

--- src/laravel/routes/web.php
+++ src/laravel/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $posts = DB::table('posts')->get();
+    return view('welcome')->with('posts', $posts);
 });
