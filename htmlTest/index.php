08c77be11f16d943ee8e99722edbbf71990d6e3d
h0useofdupree <joel.riekemann@gmail.com>
2021-06-14 11:02:26 +0200

    renamed Folder

---
+++ htmlTest/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+	<section>
+		<?php include_once("header.php") ?>
+	</section>
+</head>
+<body>
+
+	<?php include_once("home.php") ?>
+
+</body>
+</html>
