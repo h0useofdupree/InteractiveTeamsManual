5addac33b076ea68059a5910ec7ca2c65f6327d1
h0useofdupree <joel.riekemann@gmail.com>
2021-06-21 18:18:33 +0200

    clean up

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,14 +1,14 @@
 <div class="topnav" id="topNav">
-    <a href="/" class=" {{ request()->is('/') ? 'active' : 'links' }} ">
+    <a href="/" class="{{ request()->is('/') ? 'active' : 'links' }}">
         Home
     </a>
 
-    <a href="install" class=" {{ request()->is('install') ? 'active' : 'links' }} ">
+    <a href="install" class="{{ request()->is('install') ? 'active' : 'links' }}">
         Installation
     </a>
     <a href="tabs">Tabs</a>
     <a href="pers">Personalisierung</a>
-    <a href="about" class=" {{ request()->is('about') ? 'active' : 'links' }} ">
+    <a href="about" class="{{ request()->is('about') ? 'active' : 'links' }}">
         About
     </a>
     <input type="text" placeholder="Suchen.." id="suchbar">
