7a17518b7335790d033645d7fe66ab21587def76
h0useofdupree <joel.riekemann@gmail.com>
2021-06-25 16:52:28 +0200

    stuff

--- resources/views/other/about.blade.php
+++ resources/views/other/about.blade.php
@@ -5,16 +5,20 @@
         <div class="title">
             <h1>Über dieses Projekt</h1>
         </div>
-        <div class="content">
-            <h3>
-                (I)nteractive (T)eams (M)anual ist unsere Lösung, für alle Fragen und Probleme bezüglich Microsoft Teams
-            </h3><br>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde tempore qui tenetur voluptatem quo magni eius,
-                dolorem maiores commodi. Aliquid commodi necessitatibus dolor, quo consectetur unde atque eum culpa placeat
-                architecto blanditiis obcaecati quos rem officia at repudiandae laboriosam facilis dicta animi eos. Quo
-                dignissimos itaque ut perferendis dicta natus.
-            </p>
-        </div>
+        <center>
+            <div class="content">
+                <h3>
+                    (I)nteractive (T)eams (M)anual ist unsere Lösung, für alle Fragen und Probleme bezüglich Microsoft Teams
+                </h3><br>
+                <p class="par">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde tempore qui tenetur voluptatem quo magni
+                    eius,
+                    dolorem maiores commodi. Aliquid commodi necessitatibus dolor, quo consectetur unde atque eum culpa
+                    placeat
+                    architecto blanditiis obcaecati quos rem officia at repudiandae laboriosam facilis dicta animi eos. Quo
+                    dignissimos itaque ut perferendis dicta natus.
+                </p>
+            </div>
+        </center>
     </div>
 @endsection
